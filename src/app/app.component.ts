eb8a0b34da757d0e3b08b123e3bc6ddc52335221
Dhruvil <Dhruvil@buildesk.in>
2025-06-09 11:38:53 +0530

    exercise section added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { BucketComponent } from './components/bucket/bucket.component';
 import { GroceryComponent } from './components/grocery/grocery.component';
 import { Store } from '@ngrx/store';
@@ -9,7 +9,7 @@ import { groceryAction } from './store/actions/grocery.action';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BucketComponent, GroceryComponent],
+  imports: [RouterOutlet, BucketComponent, GroceryComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { ExercisesComponent } from './exercises/exercises.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent
+    },
+    {
+        path: 'exercises',
+        component: ExercisesComponent
     }
 ];
