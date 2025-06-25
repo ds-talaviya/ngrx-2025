f14900fe73cf45a68563f621558d3f75963ad3a6
Dhruvil <Dhruvil@buildesk.in>
2025-06-25 11:48:25 +0530

    ngrx-store-localstorage implemented for exe1, exe2 to store data in local storage

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,19 +1,30 @@
 import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
-import { provideStore } from '@ngrx/store';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { NgxLoadingModule } from 'ngx-loading';
+import { provideToastr } from 'ngx-toastr';
+// ngrx packages
+import { MetaReducer, provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
-import { provideToastr } from 'ngx-toastr';
-import { provideAnimations } from '@angular/platform-browser/animations';
-
+import { provideRouterStore, routerReducer } from '@ngrx/router-store';
+import { localStorageSync } from 'ngrx-store-localstorage';
+// local imports
+import { routes } from './app.routes';
 import { groceryReducer } from './store/reducers/grocery.reducer';
 import { bucketReducer } from './store/reducers/bucket.reducer';
 import { GroceryEffects } from './store/effects/grocery.effect';
-import { NgxLoadingModule } from 'ngx-loading';
-import { provideRouterStore, routerReducer } from '@ngrx/router-store';
 
+// below change is use for store data in local storage
+export function localStorageSyncReducer(reducer: any): any {
+  return localStorageSync({
+    keys: ['task', 'projects', 'tasks', 'users'],
+    storage: sessionStorage,
+    rehydrate: true
+  })(reducer);
+}
+export const metaReducers: MetaReducer[] = [localStorageSyncReducer];
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -25,6 +36,8 @@ export const appConfig: ApplicationConfig = {
       groceries: groceryReducer,
       bucket: bucketReducer,
       router: routerReducer
+    }, {
+      metaReducers
     }),
     provideEffects(
       GroceryEffects
